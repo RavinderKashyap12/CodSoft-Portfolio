84cf9d288568aa235817d3eabc2fe5081ba78022
Ravinder Kashyap <120323053+RavinderKashyap12@users.noreply.github.com>
2023-09-07 12:27:14 +0530

    Add files via upload

---
+++ port.js
@@ -0,0 +1,15 @@
+var data = document.querySelector(".role");
+
+var typeData = new Typed(data, {
+    strings: [
+      "Software Developer",
+      "Full Stack Developer",
+      "Web Developer",
+      "Backend Developer",
+      "Coder",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
